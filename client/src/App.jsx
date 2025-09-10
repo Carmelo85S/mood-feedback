0de7d913cb0810cecfac60d9d68018e812e42928
Carmelo85S <carmelo.salis85@gmail.com>
2025-09-10 10:57:23 +0200

    start webcam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const App = () => {
+  const videoRef = useRef()
+
+  useEffect(() => {
+    // Start webcam
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch(console.error);
+  }, []);
+
   return (
     <div>
-      <h1>Mood feedback</h1>
+      <h1>Hello World</h1>
+      <video ref={videoRef} autoPlay muted playsInline width="640" height="480" />
     </div>
   )
 }
