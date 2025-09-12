d021bc125323d6b97bd19d1a55b32b21ef4b5006
Carmelo85S <carmelo.salis85@gmail.com>
2025-09-12 14:48:43 +0200

    resolve base path

--- client/src/hooks/useLoadModels.js
+++ client/src/hooks/useLoadModels.js
@@ -4,11 +4,19 @@ import * as faceapi from "face-api.js";
 const useLoadModels = () => {
   useEffect(() => {
     const loadModels = async () => {
-      await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-      await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+      const MODEL_URL = import.meta.env.BASE_URL + "models";
+
+      try {
+        await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+        await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+        console.log("Face-api models loaded successfully");
+      } catch (err) {
+        console.error("Error loading face-api models:", err);
+      }
     };
+
     loadModels();
   }, []);
-}
+};
 
-export default useLoadModels
+export default useLoadModels;
